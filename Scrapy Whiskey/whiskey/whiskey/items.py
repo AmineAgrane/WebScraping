4d10f40cb6a03894bf25bc8c83871b1f6f4f1428
AmineAgrane <agrion35@gmail.com>
2021-12-08 15:50:39 +0100

    Update on scrapy whsiky project
    adding a new parse method, and using the items class.

--- Scrapy Whiskey/whiskey/whiskey/items.py
+++ Scrapy Whiskey/whiskey/whiskey/items.py
@@ -1,12 +1,22 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+# Remove HTML Tags only.
+from w3lib.html import remove_tags
+
+# Returns the first non-null/non-empty value from the values received.
+from itemloaders.processors import TakeFirst
+
+# the first function is applied to each element. The results of these function are
+# concatenated to construct a new iterable, which is then used to apply the second function,
+# and so on, until the last function is applied to each value of the list of values
+from itemloaders.processors import MapCompose
+
+
+def remove_currency(value):
+    return value.replace('£', '').strip()
 
 
 class WhiskeyItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    price = scrapy.Field(input_processor=MapCompose(remove_tags, remove_currency), output_processor=TakeFirst())
+    link = scrapy.Field()
--- Scrapy Whiskey/whiskey/whiskey/spiders/whiskyspider.py
+++ Scrapy Whiskey/whiskey/whiskey/spiders/whiskyspider.py
@@ -1,4 +1,6 @@
 import scrapy
+from whiskey.items import WhiskeyItem
+from scrapy.loader import ItemLoader
 
 
 ###### SHELL MODE #######
@@ -14,32 +16,47 @@ import scrapy
 #  price = p.css('span.price::text').get().replace('£', '')
 #  link  = p.css('a.product-item-link').attrib['href']
 
-
 class WhiskySpider(scrapy.Spider):
-    """
-    run program  : 'scrapy crawl whisky' (in \PycharmProjects\WebScraping\Scrapy Whiskey\whiskey\)
-    export results to json file : 'scrapy crawl whisky -O whisky.json'
-    """
+    """ run program  : 'scrapy crawl whisky' (in \PycharmProjects\WebScraping\Scrapy Whiskey\whiskey\)
+    export results to json file : 'scrapy crawl whisky -O whisky.json' """
+
     # Name of the spider to differentiate it from the others
     name = "whisky"
 
     # list of urls that reprsent the starting point of the spider
     start_urls = ['https://www.whiskyshop.com/scotch-whisky/all']
 
+
     def parse(self, response):
         products = response.css('div.product-item-info')
-        for p in products:
+
+        for product in products:
+            loader = ItemLoader(item=WhiskeyItem(), selector=product)
+            loader.add_css('name', 'a.product-item-link')
+            loader.add_css('price', 'span.price')
+            loader.add_css('link', 'a.product-item-link::attr(href)')
+
+            yield loader.load_item()
+
+        next_page = response.css('a.action.next').attrib['href']
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+
+    def old_parse(self, response):
+        products = response.css('div.product-item-info')
+        for product in products:
             try:
                 yield {
-                    'title': p.css('a.product-item-link::text').get(),
-                    'price': p.css('span.price::text').get().replace('£', ''),
-                    'link': p.css('a.product-item-link').attrib['href'],
+                    'name':  product.css('a.product-item-link::text').get(),
+                    'price':  product.css('span.price::text').get().replace('£', ''),
+                    'link':   product.css('a.product-item-link').attrib['href'],
                 }
             except:
                 yield {
-                    'title': p.css('a.product-item-link::text').get(),
+                    'name': product.css('a.product-item-link::text').get(),
                     'price': 'sold out',
-                    'link': p.css('a.product-item-link').attrib['href'],
+                    'link': product.css('a.product-item-link').attrib['href'],
                 }
 
         next_page = response.css('a.action.next').attrib['href']
